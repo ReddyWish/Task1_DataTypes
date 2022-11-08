f9f2b86e7f6bbef1e7b800da18531095b4472afc
Ilya <ilya.shuvatov1@gmail.com>
2022-11-08 11:41:14 +0100

    Initial commit

---
+++ data-types.js
@@ -0,0 +1,32 @@
+const name = 'ilya';
+const age = 30;
+const likesDogs = true;
+const empty = null;
+let demonstrationOfUndefined;
+const demonstrationOfBigNum = 989897875784176767514574657495n;
+const demonstrationOfSymbol = Symbol('id');
+const demonstrationOfObject = {
+    name: 'ilya',
+    age: 30,
+    [likesDogs]: true
+}
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof likesDogs)
+console.log(typeof empty)
+console.log(typeof demonstrationOfUndefined)
+console.log(typeof demonstrationOfBigNum)
+console.log(typeof demonstrationOfSymbol)
+console.log(typeof demonstrationOfObject)
+
+
+
+
+
+
+
+
+
+
+
